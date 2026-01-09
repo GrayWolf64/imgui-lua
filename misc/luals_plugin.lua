c924b62aad8e8a43877d3b7824fd4e6aa208e7f2
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2026-01-09 20:44:37 +0800

    Update plugin

--- misc/luals_plugin.lua
+++ misc/luals_plugin.lua
@@ -1,23 +1,23 @@
 function OnSetText(uri, text)
-    if not uri:match('%.lua$') then
+    if not uri:match("%.lua$") then
         return nil
     end
 
     local diffs = {}
 
-    for kwPos, class, colon, meth, sigStart in text:gmatch '()struct_method%s+([%w_]+)(:)([%w_]+)()' do
-        local kwEnd = kwPos + #'struct_method' - 1
+    for kwPos, class, colon, meth, sigStart in text:gmatch("()struct_method%s+([%w_]+)(:)([%w_]+)()") do
+        local kwEnd = kwPos + #"struct_method" - 1
 
-        diffs[#diffs+1] = {
+        diffs[#diffs + 1] = {
             start  = kwPos,
             finish = kwEnd,
-            text   = '',
+            text   = "",
         }
 
-        diffs[#diffs+1] = {
+        diffs[#diffs + 1] = {
             start  = kwEnd + 1,
             finish = kwEnd,
-            text   = 'function ',
+            text   = "function GMetaTables.",
         }
     end
 
