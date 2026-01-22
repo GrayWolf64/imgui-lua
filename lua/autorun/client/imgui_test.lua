7929b66d0a34c08d146fb2dc1a621fa5524f4697
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2026-01-22 23:56:37 +0800

    Update test

--- lua/autorun/client/imgui_test.lua
+++ lua/autorun/client/imgui_test.lua
@@ -5,6 +5,11 @@ ImGui.CreateContext()
 
 ImGui_ImplGMOD.Init()
 
+local g = ImGui.GetCurrentContext()
+
+local window1_open = {true}
+local window2_open = {true}
+
 --- TODO: can i actually switch different hooks dynamically to achieve our windows rendered under and above the game ui or derma?
 hook.Add("PostRender", "ImGuiTest", function()
     cam.Start2D()
@@ -13,17 +18,20 @@ hook.Add("PostRender", "ImGuiTest", function()
 
     ImGui.NewFrame()
 
-    ImGui.PushFont(nil, 40) -- math.max(20, math.abs(100 * math.sin(SysTime())))
+    -- math.max(20, math.abs(100 * math.sin(SysTime())))
+    ImGui.PushFont(nil, 30)
 
-    local window1_open = {true}
-    ImGui.Begin("Hello World!", window1_open)
-    ImGui.Text("WTF")
-    ImGui.Text("A Quick Brown Fox Jumps Over a Lazy Bear!")
+    ImGui.SetNextWindowSize(ImVec2(550, 400), ImGuiCond_FirstUseEver)
+
+    ImGui.Begin("Hello, World!", window1_open)
+        ImGui.Text("Lua Memory Usage: %dKb", math.Round(collectgarbage("count")))
+        ImGui.Text("FPS: %d", g.IO.Framerate)
     ImGui.End()
 
     ImGui.PopFont()
 
-    local window2_open = {true}
+    ImGui.SetNextWindowPos(ImVec2(30, 100), ImGuiCond_FirstUseEver)
+
     ImGui.Begin("ImGui Demo", window2_open)
     ImGui.End()
 
@@ -32,16 +40,6 @@ hook.Add("PostRender", "ImGuiTest", function()
     ImGui.Render()
 
     ImGui_ImplGMOD.RenderDrawData(ImGui.GetDrawData())
-
-    -- Temporary
-    local g = ImGui.GetCurrentContext()
-    draw.DrawText(
-        string.format(
-            "Mem: %dkb\nFramerate: %d",
-            math.Round(collectgarbage("count")),
-            g.IO.Framerate
-        ), "CloseCaption_Bold", 800, 800, color_white
-    )
 
     cam.End2D()
 end)
